d37e64ccd2cda6826838020cf42130307d5eeaef
ACER\khoav <khoa93076@donga.edu.vn>
2024-10-29 16:46:31 +0700

    Bài tập buổi 3

--- src/main/java/com/example/baitap2/model/UserModel.java
+++ src/main/java/com/example/baitap2/model/UserModel.java
@@ -1,26 +1,36 @@
-package com.example.baitap2;
+package com.example.baitap2.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "User_Demo")
 public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private int id;
+    @Column
     private String firstName;
+    @Column
     private String lastName;
 
-    // Constructor không tham số
-    public UserModel() {
+    public int getId() {
+        return id;
     }
 
-    // Getters và setters cho các thuộc tính
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
